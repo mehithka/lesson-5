dc969fe675d9b6a494c0f7b6d59544db13400870
mehithka <mehith.karacasia@gmail.com>
2024-12-12 15:39:05 +0500

    a3

---
+++ A3.py
@@ -0,0 +1,9 @@
+num = int(input("enter a whole number"))
+
+if num>1063:
+    print("Wow you wrote a number bigger than a decilion!")
+    print("i am in the if block")
+else :
+    print("Haha There is no way you can type a decillion here!")
+    print("i am in the else block")
+print("i am not int the if nor the else block")
